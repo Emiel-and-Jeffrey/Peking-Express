aa2cbad917f8592cbd1da9fb207c606e445cd416
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2021-03-18 10:58:36 +0100

    Implement service in controller

--- src/controllers/peeking.controller.ts
+++ src/controllers/peeking.controller.ts
@@ -1,35 +1,39 @@
-import { Body, Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from "@nestjs/common";
 import { PeekingService } from "services/peeking.service";
 import { Schemas } from "constants/schemas";
 import { IMapRequest } from "../interfaces";
+import { GameMap } from "../classes/game-map.class";
 
 @Controller(Schemas.exampleSchema)
 export class PeekingController
 {
     constructor(private readonly service: PeekingService) { }
 
-    @Get("/initialize")
-    public initializeMap(@Body() map: IMapRequest): IMapRequest
+    @Post("/initialize/:budget")
+    public initializeMap(@Body() map: IMapRequest,
+                         @Param("budget", ParseIntPipe) budget: number): GameMap
     {
-        return map;
+        return this.service.InitializeMap(map, budget);
     }
 
-    @Get("/setStartLocation/:startLocation")
-    public setStartLocation(@Param("startLocation", ParseIntPipe) startLocation: number): number
+    @Get("/setStartLocation/:startLocation/:playerID")
+    public setStartLocation(@Param("startLocation", ParseIntPipe) startLocation: number,
+                            @Param("playerID", ParseIntPipe) playerID: number): void
     {
-        return startLocation;
+        this.service.SetStartLocation(playerID, startLocation);
     }
 
     @Get("/updateCompetitorLocation/:groupID/:location")
     public updateCompetitorLocation(@Param("groupID", ParseIntPipe) groupID: number,
-                                    @Param("location", ParseIntPipe) location: number): number
+                                    @Param("location", ParseIntPipe) location: number): void
     {
-        return groupID;
+        this.service.UpdateCompetitorLocation(groupID, location);
     }
 
-    @Get("/nextMove")
-    public nextMove(): number
+    @Get("/nextMove/:target")
+    public nextMove(@Param("target", ParseIntPipe) target: number): number
     {
-        return 0;
+        const node = this.service.NextMove(target);
+        return node === undefined ? -1 : node.ID;
     }
 }
